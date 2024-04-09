c3f305c590bca17e2a5a77b8c3dbe7ca8efb4efd
Sandra Klocinski <sklocins@umich.edu>
2024-04-09 03:20:53 -0400

    unsure

--- js/script.js
+++ js/script.js
@@ -37,6 +37,46 @@
 //     dots[slideIndex-1].className += " active";
 //     captionText.innerHTML = dots[slideIndex-1].alt;
 // }
+// Open the Modal
+function openModal1() {
+    document.getElementById("myModal1").style.display = "block";
+}
+
+// Close the Modal
+function closeModal1() {
+    document.getElementById("myModal1").style.display = "none";
+}
+
+var slideIndex1 = 1;
+showSlides1(slideIndex1);
+
+// Next/previous controls
+function plusSlides1(n) {
+    showSlides1(slideIndex1 += n);
+}
+
+// Thumbnail image controls
+function currentSlide1(n) {
+    showSlides1(slideIndex1 = n);
+}
+
+function showSlides1(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides1");
+    var dots = document.getElementsByClassName("demo1");
+    var captionText = document.getElementById("caption1");
+    if (n > slides.length) {slideIndex1 = 1}
+    if (n < 1) {slideIndex1 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex1-1].style.display = "block";
+    dots[slideIndex1-1].className += " active";
+    captionText.innerHTML = dots[slideIndex1-1].alt;
+}
 
 // Open the Modal
 function openModal2() {
